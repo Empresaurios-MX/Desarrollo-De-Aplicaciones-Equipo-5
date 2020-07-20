382732b5f524eb05ac12e518ef89cccb1a21c49a
Daniel Antonio Nolasco Alvarado <dnieln7@gmail.com>
2020-07-20 15:03:13 -0500

    se agrego prototipo flutter

---
+++ Desarrollo/Flutter - Prototipo/empresa_uv/lib/src/app.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:empresauv/src/ui/profile/profile_add.dart';
+import 'package:empresauv/src/ui/home/home_screen.dart';
+
+GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        accentColor: Colors.green,
+        canvasColor: Colors.white
+      ),
+      home: Scaffold(
+        key: _scaffoldState,
+        appBar: AppBar(
+          title: Text(
+            "Empresa UV",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          actions: <Widget>[
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  _scaffoldState.currentContext,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                    return ProfileAddScreen();
+                  }),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: HomeScreen(),
+      ),
+    );
+  }
+}
---
+++ Desarrollo/Flutter - Prototipo/empresa_uv/lib/src/ui/home/home_screen.dart
@@ -0,0 +1,141 @@
+import 'package:empresauv/src/service/profile_service.dart';
+import 'package:empresauv/src/ui/profile/profile_add.dart';
+import 'package:flutter/material.dart';
+import 'package:empresauv/src/model/profile.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  BuildContext context;
+  ProfileService profileService;
+
+  @override
+  void initState() {
+    super.initState();
+    profileService = ProfileService();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    this.context = context;
+    return SafeArea(
+      child: FutureBuilder(
+        future: profileService.getProfiles(),
+        builder: (BuildContext context, AsyncSnapshot<List<Profile>> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                  "Ha ocurrido un error: ${snapshot.error.toString()}"),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            List<Profile> profiles = snapshot.data;
+            return _buildListView(profiles);
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildListView(List<Profile> profiles) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          Profile profile = profiles[index];
+          return Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      profile.name,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    Text(profile.email),
+                    Text(profile.age.toString()),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        FlatButton(
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text("Alerta"),
+                                    content: Text(
+                                        "¿Está seguro de borrar: ${profile.name}?"),
+                                    actions: <Widget>[
+                                      FlatButton(
+                                        child: Text("Sí"),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                          profileService
+                                              .deleteProfile(profile.id)
+                                              .then((isSuccess) {
+                                            if (isSuccess) {
+                                              setState(() {});
+                                              Scaffold.of(this.context)
+                                                  .showSnackBar(SnackBar(
+                                                      content: Text(
+                                                          "Borrado con éxito")));
+                                            } else {
+                                              Scaffold.of(this.context)
+                                                  .showSnackBar(SnackBar(
+                                                      content: Text(
+                                                          "Error al borrar")));
+                                            }
+                                          });
+                                        },
+                                      ),
+                                      FlatButton(
+                                        child: Text("No"),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                      )
+                                    ],
+                                  );
+                                });
+                          },
+                          child: Text(
+                            "Borrar",
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ),
+                        FlatButton(
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return ProfileAddScreen(profile: profile);
+                            }));
+                          },
+                          child: Text(
+                            "Editar",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: profiles.length,
+      ),
+    );
+  }
+}
